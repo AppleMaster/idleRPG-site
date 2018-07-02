e22262815f6e3cb86eb40ce0b07ba3f9ddfb9160
Apple_Master <apples@sorcery.net>
2018-07-02 15:38:34 +0200

    Rar

---
+++ config.php
@@ -0,0 +1,45 @@
+<?php
+
+$admin_email="applemaster@sorcery.net";
+$admin_nick="Apple_Master";
+
+// nickname of your bot
+$irpg_bot="IdleRPG";
+
+// your game's server
+$irpg_network="irc.sorcery.net";
+
+// your game's channel
+$irpg_chan="#IdleRPG";
+
+// full or relative pathname to the DBs:
+
+// character database
+$irpg_db="/home/irpg/irpg.db";
+
+// item database
+$irpg_itemdb="../mapitems.db";
+
+// time modifiers file
+$irpg_mod="/home/irpg/modifiers.txt";
+
+// active quest info file
+$irpg_qfile="/home/irpg/questinfo.txt";
+
+// image to use for the top logo
+$irpg_logo="idlerpg.png";
+
+// directory in which your site is located from the root directory. my site
+// is http://jotun.ultrazone.org/g7/, so it's "/g7/"
+$BASEURL="/";
+
+// width-wise dimension of your map file
+$mapx = 500;
+
+// length-wise dimension of your map file
+$mapy = 500;
+
+// size of crosses for players and items on world map
+$crosssize = 3;
+
+?>
